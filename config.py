48905f57879e2d604a88fed1b63791d4250f3d5c
144parthi-maker <144parthi@gmail.com>
2025-08-19 21:08:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/144parthi-maker/Rudra",
+    "https://github.com/144parthi-maker/Rudra-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
